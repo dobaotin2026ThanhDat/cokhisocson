0be1b179b96b862bdb174a22cf86842fbcecefa3
baotin19912026 <nhatanquanghanh2022@gmail.com>
2026-03-17 18:35:15 +0700

    add custom domain

--- thanhdat_web/settings.py
+++ thanhdat_web/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-nb=-r-1xz#2p-qu0wriqv@0)z$n#0_$1f2v*x9+o5swc%4$*bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'cokhisocson.io.vn',
+    'www.cokhisocson.io.vn',
+    'cokhisocson.onrender.com'
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
