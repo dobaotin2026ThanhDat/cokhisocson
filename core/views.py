1ab4211adc64074a7b07ba9062291a4f008db180
baotin19912026 <nhatanquanghanh2022@gmail.com>
2026-03-19 17:05:46 +0700

    Thay template tĩnh mới, update static, css/js/images

--- core/views.py
+++ core/views.py
@@ -1,31 +1,5 @@
+
 from django.shortcuts import render
 
-
 def home(request):
-
-    return render(request,"home/home.html")
-
-
-def about(request):
-
-    return render(request,"pages/about.html")
-
-
-def profile(request):
-
-    return render(request,"pages/profile.html")
-
-
-def quote(request):
-
-    return render(request,"pages/quote.html")
-
-
-def services(request):
-
-    return render(request,"services/services.html")
-
-
-def projects(request):
-
-    return render(request,"projects/projects.html")
+    return render(request, 'home.html')
--- thanhdat_web/settings.py
+++ thanhdat_web/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nb=-r-1xz#2p-qu0wriqv@0)z$n#0_$1f2v*x9+o5swc%4$*bh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['127.0.0.1',
     'cokhisocson.io.vn',
     'www.cokhisocson.io.vn',
     'cokhisocson.onrender.com'
@@ -124,6 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     'static',
+]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
